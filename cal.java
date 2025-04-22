3561dc5b79cad9116d015ace9eda655f5ad9bc35
Usha-U-G <ushagondakar@gmail.com>
2025-04-23 03:52:43 +0530

    code pushed 2

---
+++ cal.java
@@ -0,0 +1,27 @@
+package jenkinsjava;
+ public class cal {
+    public static void main(String[] args) {
+       
+        int num1 = 10;
+        int num2 = 5;
+        
+      
+        int sum = num1 + num2;
+        int sub = num1 - num2;
+        int multi = num1 * num2;
+        int divide = num1 / num2;
+        int mod = num1 % num2;
+
+      
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + sub);
+        System.out.println("multiple: " + multi);
+        System.out.println("divide: " + divide);
+        System.out.println("modul: " + mod);
+    }
+}
+ 
+    
+
---
+++ inherit.java
@@ -0,0 +1,23 @@
+package jenkinsjava;
+
+class car {
+    void show() {
+        System.out.println("This is me car .");
+    }
+}
+
+class bike extends car {
+    void display() {
+        System.out.println("This is me bike");
+    }
+}
+
+public class inherit {
+    public static void main(String[] args) {
+        bike b = new bike();
+
+       b.display();
+       b.show();
+    }
+}
+
